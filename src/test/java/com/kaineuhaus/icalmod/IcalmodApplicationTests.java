b98f909cb1627c6bc0c016f560462634add461ae
Kai Neuhaus <code@kaineuhaus.com>
2022-02-09 21:49:34 +0100

    Update IcalmodApplicationTests.java

--- src/test/java/com/kaineuhaus/icalmod/IcalmodApplicationTests.java
+++ src/test/java/com/kaineuhaus/icalmod/IcalmodApplicationTests.java
@@ -1,17 +1,8 @@
 package com.kaineuhaus.icalmod;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IcalmodApplicationTests {
-
-    /*
-    @Test
-    void contextLoads() {
-
-    }
-
-     */
 
 }
